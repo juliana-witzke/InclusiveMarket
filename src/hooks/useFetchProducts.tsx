b3a2da44a4aa4febd196fb05657836b3821d9d1c
Ricardo Morato <ricardomoratodev@gmail.com>
2022-05-13 11:30:33 -0300

    refactor: return error message on useFetchProducts

--- src/hooks/useFetchProducts.tsx
+++ src/hooks/useFetchProducts.tsx
@@ -1,15 +1,15 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { useEffect, useState } from 'react'
 
 export const useFetchProducts = () => {
   const [products, setProducts] = useState<IProduct[]>([])
-  const [error, setError] = useState(false)
+  const [error, setError] = useState<string | undefined>(undefined)
 
   useEffect(() => {
     axios
       .get('http://localhost:3000/api/products')
       .then(response => setProducts(response.data.products))
-      .catch(() => setError(true))
+      .catch((error: AxiosError) => setError(error.response?.data.error))
   }, [])
 
   return {
