6be5c6940ba0f65943b78aca715d9707a62a41fe
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2022-05-23 10:56:18 -0300

    refactor: get available product and render before all tests

--- src/components/ProductCard/__tests__/product-card.spec.tsx
+++ src/components/ProductCard/__tests__/product-card.spec.tsx
@@ -9,17 +9,20 @@ import { startMirageServer } from '../../../miragejs/server'
 
 describe("<ProductCard />", () => {
     let server: Server
+    let availableProduct: IProduct;
     const fetchProducts = async () => {
         const { result: productsResult, waitForNextUpdate } =
             renderHook(useFetchProducts)
         await waitForNextUpdate()
         return productsResult.current.products
     }
+    
 
     beforeAll(async () => {
         server = startMirageServer({ environment: 'test' })
-    
         server.createList('product', 5)
+        availableProduct = (await fetchProducts())[0]
+        render(<ProductCard product={availableProduct} />);
     })
     
     afterAll(() => {
@@ -27,12 +30,15 @@ describe("<ProductCard />", () => {
     })
 
     it('should render the product card component with product info', async () => {
-        const availableProduct = (await fetchProducts())[0]
-        render(<ProductCard product={availableProduct} />);
+
         expect(screen.getByText(availableProduct.title)).toBeInTheDocument();
         const img = screen.getByAltText(availableProduct.image.description);
         expect(img).toBeInTheDocument()
         expect(img).toHaveAttribute("src", availableProduct.image.url)
         expect(screen.getByText(availableProduct.price, { exact: false })).toBeInTheDocument();
     })
+
+    // it("should call add product to cart function when clicking + button", () => {
+        
+    // })
 })
